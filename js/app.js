e3fafbc321b5de56a403f2c9bdb1b21ba8f213f4
giselicosta <gi.costa03@hotmail.com>
2018-07-31 17:29:02 -0300

    Atualizado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-// La funcionalidad de tu proyecto
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(6.481831, -80.825333),
+    zoom: 2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}// La funcionalidad de tu proyecto
